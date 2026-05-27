2d0ff9a635185fd3e091cd6f16c2af12cd0489e2
Hendrik Hömberg <hendrik.hoemberg@web.de>
2026-05-27 11:59:42 +0200

    refactor(wizard): source picker first, mode picker second

--- src/main/java/com/HendrikHoemberg/StudyHelper/dto/StudyMode.java
+++ src/main/java/com/HendrikHoemberg/StudyHelper/dto/StudyMode.java
@@ -1,8 +1,12 @@
 package com.HendrikHoemberg.StudyHelper.dto;
 
 public enum StudyMode {
-    FLASHCARDS,
-    QUIZ,
-    EXAM,
-    DUNGEON
+    FLASHCARDS(1),
+    QUIZ(1),
+    EXAM(1),
+    DUNGEON(DungeonSize.SMALL.totalPrompts());
+
+    private final int minimumCardsRequired;
+    StudyMode(int minimumCardsRequired) { this.minimumCardsRequired = minimumCardsRequired; }
+    public int minimumCardsRequired() { return minimumCardsRequired; }
 }
